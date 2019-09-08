5626060d2063400592a26c540cd79f86d375d9ee
niel2018 <galaxy_neil@hotmail.com>
2019-09-08 17:34:39 +0800

    add stock plot

--- test.py
+++ test.py
@@ -6,18 +6,7 @@ sys.path.append(r'./StockAnalyzer')
 import ImportStock as Sa
 import BaiduIndexCrawler as BIC
 
-
-# Test code here
-def main():
-    # stock_list = ['600000', '601666', '600111', '000783', '002594', '300024', '600000', '600021', '300258']
-    # stock_list = ['002594']
-
-    # stock_list_len = len(stock_list)
-    #for i in range(stock_list_len):
-    #    Sa.calc_best_ma_trade_strategy(stock_list[i], '20170615', '20170730', 30, 30, 10000)
-    # Sa.calc_best_macd_trade_strategy(stock_list[i], '2018-06-30', '2019-07-05', 10000)
-
-    # BIC.GetBaiduIndex()
+def getAllStockData():
     ts_pro = ts.pro_api('45dba9f72d4488e265fc980ea7b70f4416b529589d46afa55540894a')
     stock_list = ts_pro.query('stock_basic', exchange='', list_status='L',
                               fields='ts_code,symbol,name,area,industry,list_date')
@@ -33,6 +22,19 @@ def main():
         network_data[['open', 'close', 'high', 'low']] = network_data[['open', 'close', 'high', 'low']].astype(
             np.float32)
         network_data.to_csv(stock_file, index=False)
+
+
+# Test code here
+def main():
+    # stock_list = ['600000', '601666', '600111', '000783', '002594', '300024', '600000', '600021', '300258']
+    stock_list = ['002594']
+
+    stock_list_len = len(stock_list)
+    for i in range(stock_list_len):
+        Sa.calc_best_ma_trade_strategy(stock_list[i], '20170630', '20170730', 30, 30, 10000)
+        Sa.calc_best_macd_trade_strategy(stock_list[i], '2018-06-30', '2019-07-30', 10000)
+
+    # BIC.GetBaiduIndex()
 
 
 main()
